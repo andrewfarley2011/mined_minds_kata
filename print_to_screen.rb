44a667f535c80575c64b2a588305171af98e482e
andrewfarley2011 <andrewfarley@yahoo.com>
2017-06-23 10:39:28 -0400

    Example 4b

--- print_to_screen.rb
+++ print_to_screen.rb
@@ -5,12 +5,12 @@ x = 0
 100.times do |x|
 x += 1
 
-if x % 15 == 0
-	print "mined_minds"
-elsif x % 3 == 0
-	print "mined"
-elsif x % 5 == 0
-	print "minds"
+if (x % 15 == 0)
+	p "mined_minds"
+elsif (x % 3 == 0)
+	p "mined"
+elsif (x % 5 == 0)
+	p "minds"
 else
 	puts x
 end
